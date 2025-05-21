66b2de0d560a3d7217768b3cff305abd9feb2425
Drew Barkman <djbarkman25@gmail.com>
2025-05-21 11:28:24 -0500

    middle two exercises done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        let index = 0;
+        while (index !== -1) {
+            index = array.findIndex(val => val === arg)
+            if (index === -1) continue;
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") return 'ERROR';
+    if (!(Number.isInteger(start) && Number.isInteger(end))) return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
 
+    let a, b;
+    if (start < end) {
+        a = start;
+        b = end;
+    } else {
+        a = end;
+        b = start;
+    }
+
+    result = 0;
+    for (; a <= b; a++) {
+        result += a;
+    }
+    return result;
 };
 
 // Do not edit below this line
