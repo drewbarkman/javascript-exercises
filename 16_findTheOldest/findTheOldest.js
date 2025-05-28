8b77aed1c512fd5b948d7c59e0a03b69a2727ec3
Drew Barkman <djbarkman25@gmail.com>
2025-05-27 23:32:43 -0500

    exer 14 and 15 done!

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        return new Date().getFullYear() - birth;
+    } else {
+        return death - birth;
+    }
+}
 
-};
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+            const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+            const currAge = getAge(curr.yearOfBirth, curr.yearOfDeath);
+
+            return oldestAge < currAge ? curr : oldest;
+        })
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
