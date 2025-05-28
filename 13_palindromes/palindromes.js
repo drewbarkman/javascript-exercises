56f56969926f4d884ccdd30d223312ea80099554
Drew Barkman <djbarkman25@gmail.com>
2025-05-27 23:10:17 -0500

    exer 12-14 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    cleanString = phrase.toLowerCase()
+    .split('')
+    .filter((letter) => alphanumerical.includes(letter))
+    .join('');
+
+    reversedString = cleanString.split('').reverse().join('');
+
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (+index === NaN || +index < 0) return 'OOPS';
 
+    let firstNum = 0;
+    let secondNum = 1;
+    let currentNum = 1;
+
+    if (+index === 0) return 0;
+
+    for (let i = 1; i < +index; i++) {
+        currentNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = currentNum;
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
